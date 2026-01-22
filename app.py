611c40cd9437ce7d27715891a19e81c659bb07fb
jorgesierra2212 <jsierra@enersinc.com>
2026-01-21 21:13:27 -0500

    app.py

--- app.py
+++ app.py
@@ -2,13 +2,19 @@ import dash
 from dash import dcc, html
 from dash.dependencies import Input, Output
 import plotly.graph_objs as go
-from pydataxm import ReadDB
 import pandas as pd
 import datetime
 
-# --- CONFIGURACIÓN PARA EL SERVIDOR ---
+# Intento de importación robusta para pydataxm
+try:
+    from pydataxm import ReadDB
+except ImportError:
+    # Algunas versiones de la librería requieren este camino
+    from pydataxm.pydataxm import ReadDB
+
+# Configuración de la App
 app = dash.Dash(__name__)
-server = app.server  # <--- ESTA LÍNEA ES OBLIGATORIA PARA LA NUBE
+server = app.server  # Esto es lo que gunicorn app:server busca
 app.title = "Demanda XM Tiempo Real"
 
 # (El resto del código de la interfaz y callbacks es el mismo que el anterior)
